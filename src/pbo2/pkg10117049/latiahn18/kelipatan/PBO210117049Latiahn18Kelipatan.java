cac47310c6f0c3e4b8b21e78850f5a597abf2979
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-10-01 12:10:12 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latiahn18/kelipatan/PBO210117049Latiahn18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latiahn18.kelipatan;
+
+/**
+ *
+ * @author Gery Akbar Fauzi 
+ * Nama : Gery Akbar Fauzi 
+ * Kelas : PBO2 
+ * Nim : 10117049
+ * Deskripsi Program : Program ini berisi program untuk menghitung kelipatan
+ */
+public class PBO210117049Latiahn18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i;
+
+        //Proses Kelipatan
+        for (i = 3.5; i <= 35; i = i + 3.5) {
+            //Menampilkan kelipatan
+            System.out.println(i);
+        }
+    }
+
+}
